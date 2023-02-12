1f8944ffd648b9fde10320d944c39308c453b2ee
Ilya <irracli@gmail.com>
2023-02-12 21:44:53 +0400

    add server shutting down

--- server/server.go
+++ server/server.go
@@ -1,9 +1,12 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 
 	"github.com/klimovI/go_tickers_rates/server/handler"
 	"github.com/klimovI/go_tickers_rates/server/service"
@@ -20,9 +23,23 @@ func StartServer() {
 		Handler: handlers.Init(),
 	}
 
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatalf("Error starting server: %s", err.Error())
-	}
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatalf("Error starting server: %s", err.Error())
+		}
+	}()
 
 	fmt.Println("Listening at port " + port)
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	fmt.Println("Shutting down")
+
+	if err := server.Shutdown(context.Background()); err != nil {
+		log.Fatalf("Error shutting down: %s", err.Error())
+	}
+
+	os.Exit(0)
 }
